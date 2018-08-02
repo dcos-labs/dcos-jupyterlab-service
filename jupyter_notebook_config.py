a73ebea900a660f39884ee19b44284f008e02943
Vishnu Mohan <vishnu@mesosphere.com>
2018-08-02 02:22:54 -0400

    Set a sane base path for .ivy

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -216,6 +216,10 @@ if jupyter_conf_urls:
 if os.path.exists('krb5.conf'):
     copyfile('krb5.conf', '/etc/krb5.conf')
 
+# Set a sane base path for .ivy2
+spark_opts.append('--conf spark.jars.ivy={}/.ivy2'.format(
+    os.getenv('MESOS_SANDBOX', '/home/beakerx')))
+
 # Accumulate Spark --conf properties specified in SPARK_CONF_<> env vars
 spark_conf_env_pattern = re.compile(r'^SPARK_CONF')
 spark_conf_envs = [env for env in os.environ if spark_conf_env_pattern.match(env)]
